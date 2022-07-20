e3dfa2490128180fc88263f97e71f28c7b0de8e9
harshsiriah <harsh@plgworks.com>
2022-07-20 19:56:36 +0530

    PR review changes

--- src/utils/LocalEvent.js
+++ src/utils/LocalEvent.js
@@ -4,4 +4,11 @@ export const LocalEvent = new EventEmitter();
 
 export const EventTypes = {
   SwitchToHomeStack: 'event_types_switch_to_home_stack',
+  BottomSheets: {
+    CloseBottomSheet: 'event_types_bottom_sheets_close_bottom_sheet',
+  },
+  Internet: {
+    Connected: 'event_types_internet_connected',
+    Disconnected: 'event_types_internet_disconnected',
+  },
 };

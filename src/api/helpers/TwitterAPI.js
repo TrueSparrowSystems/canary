8336ae18a1afb554fed1b23e0eb193bc34ff6e6f
Vinay Harwani <vinay@plgworks.com>
2022-07-21 13:55:16 +0530

    Added Tweets Collection page and api integration

--- src/api/helpers/TwitterAPI.js
+++ src/api/helpers/TwitterAPI.js
@@ -2,6 +2,7 @@ import {APIService} from '../../services/Api';
 
 const EndPoints = {
   timelineFeed: 'https://api.twitter.com/2/tweets/search/recent',
+  multipleTweetsLookup: 'https://api.twitter.com/2/tweets',
 };
 class TwitterApi {
   timelineFeed(nextPageIdentifier) {
@@ -25,6 +26,28 @@ class TwitterApi {
     }
     const apiService = new APIService({});
     return apiService.get(EndPoints.timelineFeed, data);
+  }
+
+  multipleTweetLookup(ids) {
+    if (ids.length === 0) {
+      return;
+    }
+
+    const data = {
+      ids: ids,
+      expansions:
+        'attachments.media_keys,author_id,in_reply_to_user_id,geo.place_id,referenced_tweets.id',
+      'media.fields':
+        'media_key,duration_ms,height,preview_image_url,type,url,width',
+      'place.fields':
+        'contained_within,country,country_code,full_name,geo,id,name,place_type',
+      'tweet.fields':
+        'attachments,conversation_id,author_id,context_annotations,created_at,entities,geo,id,in_reply_to_user_id,referenced_tweets,source,text',
+      'user.fields': 'id,name,profile_image_url,username,verified',
+    };
+
+    const apiService = new APIService({});
+    return apiService.get(EndPoints.multipleTweetsLookup, data);
   }
 }
 export default new TwitterApi();
---
+++ src/components/CollectionTweetList/useCollectionTweetListData.js
@@ -0,0 +1,61 @@
+import {useCallback, useState, useRef, useEffect} from 'react';
+import TwitterAPI from '../../api/helpers/TwitterAPI';
+import {collectionService} from '../../services/CollectionService';
+import {getTweetData} from '../utils/ViewData';
+
+function useCollectionTweetListData(props) {
+  const {collectionId} = props;
+  const [isLoading, setIsLoading] = useState(false);
+  const listDataRef = useRef([]);
+  const _collectionService = collectionService();
+
+  const fetchData = useCallback(() => {
+    setIsLoading(true);
+    _collectionService
+      .getCollectionDetails(collectionId)
+      .then(collectionData => {
+        var tweetIds = '';
+        var ids = collectionData?.tweetIds;
+        ids.forEach((tweetId, index) => {
+          tweetIds += tweetId.toString();
+          if (index !== ids.length - 1) {
+            tweetIds += ',';
+          }
+        });
+        TwitterAPI.multipleTweetLookup(tweetIds)
+          .then(apiResponse => {
+            var array = [];
+            const {data} = apiResponse;
+            const newData = data?.data;
+            newData.forEach(tweet => {
+              const tweetData = getTweetData(tweet, apiResponse);
+              array.push(tweetData);
+            });
+            listDataRef.current = array;
+          })
+          .finally(() => {
+            setIsLoading(false);
+          });
+      })
+      .catch(() => {
+        setIsLoading(false);
+      });
+  }, [_collectionService, collectionId]);
+
+  useEffect(() => {
+    fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const onRefresh = useCallback(() => {
+    fetchData();
+  }, [fetchData]);
+
+  return {
+    bIsLoading: isLoading,
+    fnOnRefresh: onRefresh,
+    aDataSource: listDataRef.current,
+  };
+}
+
+export default useCollectionTweetListData;

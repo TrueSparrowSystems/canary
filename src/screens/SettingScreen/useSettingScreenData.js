026445987dad34603443634d674b957f38db3c1e
Vinay Harwani <vinay@plgworks.com>
2022-09-27 16:24:24 +0530

    Updated share library

--- src/screens/SettingScreen/useSettingScreenData.js
+++ src/screens/SettingScreen/useSettingScreenData.js
@@ -1,8 +1,10 @@
 import {useNavigation} from '@react-navigation/native';
 import {useCallback} from 'react';
-import {Share} from 'react-native';
 import {Constants} from '../../constants/Constants';
 import ScreenName from '../../constants/ScreenName';
+import Share from 'react-native-share';
+import {Platform} from 'react-native';
+import {Canary} from '../../assets/common';
 
 function useSettingScreenData() {
   const navigation = useNavigation();
@@ -12,9 +14,37 @@ function useSettingScreenData() {
   }, [navigation]);
 
   const onShareAppPress = useCallback(() => {
-    Share.share({
-      message: `Check out Canary app - The incognito mode of Twitter.\n${Constants.GoogleDriveLink}`,
+    // Share.share({
+    //   message: `Check out Canary app - The incognito mode of Twitter.\n${Constants.GoogleDriveLink}`,
+    // });
+    const message = 'Canary App Share';
+    const options = Platform.select({
+      ios: {
+        activityItemSources: [
+          {
+            placeholderItem: {type: 'text', content: message},
+            item: {
+              default: {
+                type: 'text',
+                content: `Check out Canary app - The incognito mode of Twitter.\n${Constants.GoogleDriveLink}`,
+              },
+              message: null,
+            },
+            linkMetadata: {
+              title: message,
+              icon: Canary,
+            },
+          },
+        ],
+      },
+      default: {
+        title: 'Canary App Share',
+        subject: 'Canary App Share',
+        message: `Check out Canary app - The incognito mode of Twitter.\n${Constants.GoogleDriveLink}`,
+      },
     });
+
+    Share.open(options).catch(() => {});
   }, []);
 
   const onPersonalizeFeedPress = useCallback(() => {

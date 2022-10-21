ac1c21e96bc248980345e848da124b09ba1c3ae8
Vinay Harwani <vinay@plgworks.com>
2022-10-21 13:27:08 +0530

    Updated user agent

--- AppVersion.js
+++ AppVersion.js
@@ -1,1 +1,1 @@
-export const AppVersion = '1.0.12';
+export const AppVersion = '1.0.13';
--- src/services/AnalyticsService.js
+++ src/services/AnalyticsService.js
@@ -1,8 +1,12 @@
 import {PlgTracker} from 'tracker-react-native';
+import {AppVersion} from '../../AppVersion';
 
 class AnalyticsService {
   init(appIdentifier, trackerEndpoint) {
-    PlgTracker.initInstance({appIdentifier, trackerEndpoint});
+    const headerParams = {
+      headers: {'User-Agent': `canary/${AppVersion}`},
+    };
+    PlgTracker.initInstance({appIdentifier, trackerEndpoint, headerParams});
   }
 
   track(eventEntity, eventAction, extraParams) {
